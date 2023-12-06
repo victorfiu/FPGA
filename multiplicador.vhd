2429e7b99dbe9edcdd4b1c27e0998cba18c58835
João Victor Rodrigues dos Santos <67395918+victorfiu@users.noreply.github.com>
2023-12-05 23:26:37 -0300

    Create multiplicador.vhd
    process

---
+++ multiplicador.vhd
@@ -0,0 +1,17 @@
+-- Autor: João Victor Rodrigues dos Santos
+-- multiplicador de 4 bits (for loop)
+-- 2 entradas de 4 bits com uma saída de 8 bits
+-- caso a segunda entrada tenha um item com 0 vai gerar um vetor de 4 bits = 0000 
+-- +  se for 1, será igual a primeira entrada.
+-- a saída será o and com o vetor 
+
+
+--	 1010
+--	 0101
+--	 x
+--	 1010
+--	0000
+--   1010
+--  0000
+--  =
+--  0110010	
