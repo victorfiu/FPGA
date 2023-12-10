eeb1778ce1f0964d74aa9dd9fe5ce6dbfbc393ae
João Victor Rodrigues dos Santos <67395918+victorfiu@users.noreply.github.com>
2023-12-10 00:26:24 -0300

    Update and rename mult2.vhd to multplicador2.vhd

--- multplicador2.vhd
+++ multplicador2.vhd
@@ -1,13 +1,16 @@
+-- multiplicador generico (n loop) utilizando unsigned
+-- não testado
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
 
 entity mult2 is
-    generic (n  : integer := 3);
+    generic (n  : integer := 3); -- quantidade de bits
     port (
         A, B : in std_logic_vector (n downto 0);
-        P : out std_logic_vector (n*2+1 downto 0)
+        P : out std_logic_vector (n*2-1 downto 0)
     );
     end mult2;
 
@@ -15,7 +18,7 @@ architecture main of mult2 is
     
 begin
     process (A,B)
-    variable PV, BP: std_logic_vector (n*2+1 downto 0);
+    variable PV, BP: std_logic_vector (n*2-1 downto 0);
     begin
         PV := (others => '0');
         BP  := (n downto 0 => '0') & B;
@@ -23,7 +26,7 @@ begin
             if A(i) = '1' then
                 PV := PV + BP;
             end if;
-            BP := BP (n*2 downto 0) & '0';
+            BP := BP (n*2-1 downto 0) & '0';
         end loop;
         P <= PV;
     end process;
